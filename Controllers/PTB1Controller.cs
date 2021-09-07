a9d5300fa5b11754accfe30cab3b6a6a58c33a17
PhamVuHaiDuong <77629198+PhamVuHaiDuong@users.noreply.github.com>
2021-09-07 09:38:53 +0700

    Add project files.

---
+++ Controllers/PTB1Controller.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PTB1.Models;
+namespace PTB1.Controllers
+{
+    public class PTB1Controller : Controller
+    {
+        GiaiPTB1 GiaiPT = new GiaiPTB1();
+
+        // GET: PTB1
+        public ActionResult PTB1()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult PTB1(string Gta, string Gtb)
+        {
+            //nha 2 gia tri tham so gui tu client va chuyen sang kieu double
+            double SoA = Convert.ToDouble(Gta);
+            double SoB = Convert.ToDouble(Gtb);
+            //khai bao bien x
+            //ke thua phuong thuc giaiphuong trinh
+            double x = GiaiPT.GiaiPhuongTrinh(SoA, SoB);
+            ViewBag.Giatrix = x;
+            return View();
+
+        }
+    }
+}
---
+++ Models/GPTB1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PTB1.Models
+{
+        public class GiaiPTB1
+        {
+            //phuong thuc giai phuong trinh bac 1
+            //can tra ve gia tri x, kieu double
+            //co 2 he so a va b la 2 tham so dau vao cua phuong thuc
+            //kieu gia tri tra ve cua phuong thuc la double
+            public double GiaiPhuongTrinh(double SoA , double SoB )
+            {
+                double x = -SoB / SoA;
+                //gia tri tra ve cua phuong thuc (bat buoc)
+                return x;
+            }
+        }
+}
